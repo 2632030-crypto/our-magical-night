98e1a4529bb10a4dee5777a411297ea7459a1583
2632030-crypto <2632030@iub.edu.bd>
2026-08-05 21:49:34 +0600

    Add motivational message on button click

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 
+document.getElementById("handBtn").addEventListener("click", function () {
+  document.getElementById("message").innerHTML =
+    "🌙 No matter how dark life gets, I'll always hold your hand. ✨<br><br>You are stronger than you think. Keep going. ❤️";
+});
